49226b666bb969d63907dd9294a4230f4eae8978
allenkang02 <ksw8841@knou.ac.kr>
2024-10-14 16:33:08 +0900

    레이더 차트 모바일 적용

--- assets/js/components/SkillRadarChart.js
+++ assets/js/components/SkillRadarChart.js
@@ -18,9 +18,10 @@ const SkillRadarChart = () => {
       <ResponsiveContainer width="100%" height="100%">
         <RadarChart outerRadius="80%" data={data}>
           <PolarGrid />
-          <PolarAngleAxis dataKey="subject" tick={{ fontSize: '10px', fill: '#666' }} />
-          <PolarRadiusAxis angle={30} domain={[0, 100]} tick={{ fontSize: '10px' }} />
-          <Radar name="Skills" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+          <PolarAngleAxis dataKey="subject" tick={{ fontSize: '10px', fill: '#A8E1DB' }} />  {/* 텍스트 색상 변경 */}
+          <PolarRadiusAxis angle={30} domain={[0, 100]} tick={{ fontSize: '10px', fill: '#A8E1DB' }} />  {/* 텍스트 색상 변경 */}
+          <Radar name="Skills" dataKey="value" stroke="#A8E1DB" fill="#A8E1DB" fillOpacity={0.6} />  {/* 레이더 색상 변경 */}
+          <Legend />
         </RadarChart>
       </ResponsiveContainer>
     </div>
