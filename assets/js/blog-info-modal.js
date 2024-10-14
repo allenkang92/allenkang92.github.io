f7e04613e7b813fab3ab6ff5018954f476ce50cf
allenkang02 <ksw8841@knou.ac.kr>
2024-10-14 20:14:49 +0900

    수정

---
+++ assets/js/blog-info-modal.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.getElementById("blog-info-modal");
+    var btn = document.getElementById("blog-info-btn");
+    var span = document.getElementsByClassName("close")[0];
+  
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+  
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+  
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  });
