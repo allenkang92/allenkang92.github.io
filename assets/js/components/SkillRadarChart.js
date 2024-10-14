e11e3fe9caae87dc60e0b613817295f9f8b618c4
allenkang02 <ksw8841@knou.ac.kr>
2024-10-14 15:24:03 +0900

    레이더 차트 숫자 변경, 색상 변경

--- assets/js/components/SkillRadarChart.js
+++ assets/js/components/SkillRadarChart.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar, ResponsiveContainer, Legend } from 'recharts';
 
 const data = [
-  { subject: 'Data Analysis & Science', value: 85, fullMark: 100 },
+  { subject: 'Data Analysis & Science', value: 55, fullMark: 100 },
   { subject: 'Interdisciplinary Research', value: 80, fullMark: 100 },
-  { subject: 'Science Communication', value: 75, fullMark: 100 },
+  { subject: 'Science Communication', value: 80, fullMark: 100 },
   { subject: 'Databases', value: 70, fullMark: 100 },
-  { subject: 'Data Processing', value: 65, fullMark: 100 },
-  { subject: 'Programming Languages', value: 60, fullMark: 100 },
-  { subject: 'MLOps', value: 70, fullMark: 100 },
-  { subject: 'Scientific Knowledge', value: 85, fullMark: 100 },
+  { subject: 'Data Processing', value: 75, fullMark: 100 },
+  { subject: 'Programming Languages', value: 55, fullMark: 100 },
+  { subject: 'MLOps', value: 60, fullMark: 100 },
+  { subject: 'Scientific Knowledge', value: 80, fullMark: 100 },
 ];
 
 const SkillRadarChart = () => {
@@ -19,7 +19,7 @@ const SkillRadarChart = () => {
         <PolarGrid />
         <PolarAngleAxis dataKey="subject" />
         <PolarRadiusAxis angle={30} domain={[0, 100]} />
-        <Radar name="Skills" dataKey="value" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+        <Radar name="Skills" dataKey="value" stroke="#A8E1DB" fill="#A8E1DB" fillOpacity={0.6} />
         <Legend />
       </RadarChart>
     </ResponsiveContainer>
