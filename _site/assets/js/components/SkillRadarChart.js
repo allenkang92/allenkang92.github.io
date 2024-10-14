c061ade4e7b40ef19f5826f32c8ff2876ba5fa7c
allenkang02 <ksw8841@knou.ac.kr>
2024-10-14 20:51:30 +0900

    차트 수정

--- _site/assets/js/components/SkillRadarChart.js
+++ _site/assets/js/components/SkillRadarChart.js
@@ -12,9 +12,9 @@ const data = [
   { subject: 'Data Visualization', value: 75, fullMark: 100 },
 ];
 
-// 커스텀 tick 컴포넌트 생성 (두 줄로 나누기)
+// 커스텀 tick 컴포넌트 생성
 const renderCustomTick = ({ payload, x, y }) => {
-  const lines = payload.value.split(' '); 
+  const lines = payload.value.split(' ');
   return (
     <text x={x} y={y} textAnchor="middle" fill="#000000" fontSize="12px"> 
       {lines.map((line, index) => (
@@ -28,15 +28,16 @@ const SkillRadarChart = () => {
   return (
     <div style={{ width: '100%', height: '100%', minHeight: '300px', maxWidth: '600px', margin: '0 auto' }}>
       <ResponsiveContainer width="100%" height="100%">
-        <RadarChart outerRadius="70%" data={data}>  {/* outerRadius 줄임 */}
+        <RadarChart outerRadius="60%" data={data}>  {/* outerRadius 줄임 */}
           <PolarGrid />
           <PolarAngleAxis 
             dataKey="subject" 
             tick={renderCustomTick} 
-            tickMargin={80} // 간격을 더 넓게 설정
+            tickMargin={100} // tickMargin을 크게 설정
+            padding={30} // 추가 여백
           />
           <PolarRadiusAxis 
-            angle={60} // 각도 조정
+            angle={45} 
             domain={[0, 100]} 
             tick={{ fontSize: '10px', fill: '#000000', fontWeight: 'bold' }}  
           />
