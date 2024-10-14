b97c7538c3e9c33a124e9f743d43197d1192bdb5
allenkang02 <ksw8841@knou.ac.kr>
2024-10-14 20:01:07 +0900

    차트 재수정

--- assets/js/components/SkillRadarChart.js
+++ assets/js/components/SkillRadarChart.js
@@ -16,9 +16,9 @@ const data = [
 const renderCustomTick = ({ payload, x, y }) => {
   const lines = payload.value.split(' '); // 띄어쓰기 기준으로 두 줄로 나눔
   return (
-    <text x={x} y={y} textAnchor="middle" fill="#000000" fontSize="12px">  {/* 글꼴 크기 설정 */}
+    <text x={x} y={y} textAnchor="middle" fill="#A8E1DB" fontSize="12px">  {/* 글꼴 크기 설정 */}
       {lines.map((line, index) => (
-        <tspan x={x} dy={index * 20} key={index}>{line}</tspan> // 두 줄로 나누어 출력, 줄 간격 조정
+        <tspan x={x} dy={index * 22} key={index}>{line}</tspan> // 두 줄로 나누어 출력, 줄 간격 조정
       ))}
     </text>
   );
@@ -28,17 +28,17 @@ const SkillRadarChart = () => {
   return (
     <div style={{ width: '100%', height: '100%', minHeight: '300px', maxWidth: '600px', margin: '0 auto' }}>
       <ResponsiveContainer width="100%" height="100%">
-        <RadarChart outerRadius="80%" data={data}>
+        <RadarChart outerRadius="70%" data={data}>
           <PolarGrid />
           <PolarAngleAxis 
             dataKey="subject" 
             tick={renderCustomTick} // 커스텀 tick 사용 (두 줄로 나누기 및 색상 설정)
-            tickMargin={80} // 축과 텍스트 간의 간격 조정
+            tickMargin={60} // 축과 텍스트 간의 간격 조정
           />
           <PolarRadiusAxis 
             angle={45} 
             domain={[0, 100]} 
-            tick={{ fontSize: '7px', fill: '#000000', fontWeight: 'bold' }}  // 텍스트 색상 및 볼드 처리
+            tick={{ fontSize: '10px', fill: '#000000', fontWeight: 'bold' }}  // 텍스트 색상 및 볼드 처리
           />
           <Radar 
             name="Skills" 
