20cccd091fdf7ece7a332319b1f70c7466cf3dd5
allenkang02 <ksw8841@knou.ac.kr>
2024-10-14 20:20:23 +0900

    차트 수정

--- assets/js/components/SkillRadarChart.js
+++ assets/js/components/SkillRadarChart.js
@@ -16,7 +16,7 @@ const data = [
 const renderCustomTick = ({ payload, x, y }) => {
   const lines = payload.value.split(' '); // 띄어쓰기 기준으로 두 줄로 나눔
   return (
-    <text x={x} y={y} textAnchor="middle" fill="#A8E1DB" fontSize="12px">  {/* 글꼴 크기 및 색상 설정 */}
+    <text x={x} y={y} textAnchor="middle" fontSize="12px">  {/* 텍스트 색상 건드리지 않음 */}
       {lines.map((line, index) => (
         <tspan x={x} dy={index * 20} key={index}>{line}</tspan> // 두 줄로 나누어 출력, 줄 간격 조정
       ))}
@@ -33,7 +33,7 @@ const SkillRadarChart = () => {
           <PolarAngleAxis 
             dataKey="subject" 
             tick={renderCustomTick} // 커스텀 tick 사용
-            tickMargin={20} // 축과 텍스트 간의 간격 조정 (간격을 줄임)
+            tickMargin={30} // 축과 텍스트 간의 간격 조정
           />
           <PolarRadiusAxis 
             angle={45} 
