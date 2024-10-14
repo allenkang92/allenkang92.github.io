476b2318cb5205b8a507afcb6c191f0386963f36
allenkang02 <ksw8841@knou.ac.kr>
2024-10-14 17:50:23 +0900

    차트 수정

--- assets/js/components/SkillRadarChart.js
+++ assets/js/components/SkillRadarChart.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar, ResponsiveContainer, Legend } from 'recharts';
 
 const data = [
-  { subject: 'Data Analysis & Science', value: 55, fullMark: 100 },
+  { subject: 'Scientific Knowledge', value: 80, fullMark: 100 },
   { subject: 'Interdisciplinary Research', value: 80, fullMark: 100 },
-  { subject: 'Science Communication', value: 80, fullMark: 100 },
-  { subject: 'Databases', value: 70, fullMark: 100 },
-  { subject: 'Data Processing', value: 75, fullMark: 100 },
+  { subject: 'Data Analysis', value: 55, fullMark: 100 },
+  { subject: 'Data Management', value: 70, fullMark: 100 }, // Data Processing + Databases 통합
   { subject: 'Programming Languages', value: 55, fullMark: 100 },
   { subject: 'MLOps', value: 60, fullMark: 100 },
-  { subject: 'Scientific Knowledge', value: 80, fullMark: 100 },
+  { subject: 'Science Communication', value: 80, fullMark: 100 },
+  { subject: 'Data Visualization', value: 75, fullMark: 100 },
 ];
 
 const SkillRadarChart = () => {
@@ -18,9 +18,22 @@ const SkillRadarChart = () => {
       <ResponsiveContainer width="100%" height="100%">
         <RadarChart outerRadius="80%" data={data}>
           <PolarGrid />
-          <PolarAngleAxis dataKey="subject" tick={{ fontSize: '10px', fill: '#A8E1DB' }} />  {/* 텍스트 색상 변경 */}
-          <PolarRadiusAxis angle={30} domain={[0, 100]} tick={{ fontSize: '10px', fill: '#A8E1DB' }} />  {/* 텍스트 색상 변경 */}
-          <Radar name="Skills" dataKey="value" stroke="#A8E1DB" fill="#A8E1DB" fillOpacity={0.6} />  {/* 레이더 색상 변경 */}
+          <PolarAngleAxis 
+            dataKey="subject" 
+            tick={{ fontSize: '10px', fill: '#A8E1DB' }} // 텍스트 색상 변경
+          />
+          <PolarRadiusAxis 
+            angle={45} 
+            domain={[0, 100]} 
+            tick={{ fontSize: '13px', fill: '#000000', fontWeight: 'bold' }}  // 텍스트 색상 및 볼드 처리
+          />
+          <Radar 
+            name="Skills" 
+            dataKey="value" 
+            stroke="#A8E1DB" 
+            fill="#000000" 
+            fillOpacity={0.6} // 레이더 색상 및 투명도 설정
+          />
           <Legend />
         </RadarChart>
       </ResponsiveContainer>
