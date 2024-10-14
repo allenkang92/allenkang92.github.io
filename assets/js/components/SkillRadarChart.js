7c57705988dd1e89a7874dd9c4eaf6398ed41225
allenkang02 <ksw8841@knou.ac.kr>
2024-10-14 21:46:20 +0900

    차트 수정

--- assets/js/components/SkillRadarChart.js
+++ assets/js/components/SkillRadarChart.js
@@ -14,12 +14,12 @@ const data = [
 
 // 커스텀 tick 컴포넌트 생성 (두 줄로 나누기 및 패딩 추가)
 const renderCustomTick = ({ payload, x, y }) => {
-  const lines = payload.value.split(' '); // 띄어쓰기 기준으로 두 줄로 나눔
-  const padding = 15;  // 패딩 값 추가
+  const lines = payload.value.split(' ');
+  const padding = 15;
   return (
-    <text x={x} y={y - padding} textAnchor="middle" fill="#000000" fontSize="11px">  {/* 글꼴 크기 설정 및 패딩 반영 */}
+    <text x={x} y={y - padding} textAnchor="middle" fill="#000000" fontSize="11px">
       {lines.map((line, index) => (
-        <tspan x={x} dy={index * 13} key={index}>{line}</tspan> // 두 줄로 나누어 출력, 줄 간격도 조정 가능
+        <tspan x={x} dy={index * 13} key={index}>{line}</tspan>
       ))}
     </text>
   );
@@ -27,14 +27,14 @@ const renderCustomTick = ({ payload, x, y }) => {
 
 const SkillRadarChart = () => {
   return (
-    <div style={{ width: '100%', height: '100%', minHeight: '500px', maxWidth: '700px', margin: '0 auto' }}>
-      <ResponsiveContainer width="100%" height="100%">
-        <RadarChart outerRadius="75%" data={data}>  
+    <div style={{ width: '100%', height: 'auto', minHeight: '600px', maxWidth: '700px', margin: '0 auto', paddingBottom: '50px' }}>
+      <ResponsiveContainer width="100%" height={500}>  {/* 고정 높이를 사용 */}
+        <RadarChart outerRadius="65%" data={data}>  {/* outerRadius 값을 줄여 공간 확보 */}
           <PolarGrid />
           <PolarAngleAxis 
             dataKey="subject" 
             tick={renderCustomTick} 
-            tickMargin={30} 
+            tickMargin={20}  {/* 적당한 마진을 유지 */}
           />
           <PolarRadiusAxis 
             angle={45} 
