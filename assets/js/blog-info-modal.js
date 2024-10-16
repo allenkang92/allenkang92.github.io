6959093d7d7df2761e0b0fc4bec9202dfe58657e
allenkang02 <ksw8841@knou.ac.kr>
2024-10-16 15:06:16 +0900

    파일 수정

--- assets/js/blog-info-modal.js
+++ assets/js/blog-info-modal.js
@@ -3,17 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
   var btn = document.getElementById("blog-info-btn");
   var span = document.getElementsByClassName("close")[0];
 
+  // 페이지 로드 시 모달을 숨김
+  modal.style.display = "none";
+
+  // 버튼 클릭 시 모달 열기
   btn.onclick = function() {
-    modal.style.display = "block";
+      modal.style.display = "block";
   }
 
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-
+  // 모달 밖을 클릭 시 모달 닫기
   window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
   }
 });
